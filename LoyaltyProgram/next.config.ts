129aadc680b3d8a8ce727ea7a0920d547b89d78e
Abdul Wahab <absurw@gmail.com>
2025-09-13 21:18:24 +0500

    Make change in next.config.js in type error

--- LoyaltyProgram/next.config.ts
+++ LoyaltyProgram/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Other config options here
+  eslint: {
+    // Disables ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
