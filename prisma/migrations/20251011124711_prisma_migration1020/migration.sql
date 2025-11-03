a8f0e196380602f55ac1df7e4a1516316395a5d2
Abdul Wahab <absurw@gmail.com>
2025-11-03 14:21:06 +0500

    make changes in migration etc

--- prisma/migrations/20251011124711_prisma_migration1020/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `conversion` to the `LoyaltyProgram` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "LoyaltyProgram" ADD COLUMN     "conversion" JSONB NOT NULL;
