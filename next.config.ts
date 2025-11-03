d865fefe2a36b1e3e9b575c554329728e072b79d
Abdul Wahab <absurw@gmail.com>
2025-11-03 15:25:37 +0500

    Changed offer type because of updated docs requirements #2 pointCost didn't removed so we do it

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
   // Other config options here
   eslint: {
     // Disables ESLint during production builds
