ee5b4c053eb6ae41929cbbc219321e0090292f14
Abdul Wahab <absurw@gmail.com>
2025-11-21 18:05:30 +0500

    feat: Add Header component for the send email page.

--- src/app/send-email/Header.tsx
+++ src/app/send-email/Header.tsx
@@ -1,18 +1,21 @@
+import { useTranslations } from "use-intl";
+
 const Header = () => {
-    return(
-        <div className="flex items-start my-[10px] justify-between">
-          <div className="flex items-center justify-start mb-0 gap-2">
-            <img
-              src="PremiumLoyalty.png"
-              alt=""
-              className="h-[37px] w-[37px]"
-            />
-            <h2 className="text-xl sm:text-2xl font-bold text-[#2C2A25]">
-              Send an Email
-            </h2>
-          </div>
-        </div>
-    )
+  const t = useTranslations("nav");
+  return (
+    <div className="flex items-start my-[10px] justify-between">
+      <div className="flex items-center justify-start mb-0 gap-2">
+        <img
+          src="PremiumLoyalty.png"
+          alt=""
+          className="h-[37px] w-[37px]"
+        />
+        <h2 className="text-xl sm:text-2xl font-bold text-[#2C2A25]">
+          {t("sendEmail")}
+        </h2>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
